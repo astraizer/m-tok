5f9973ded7a6254371bfc511e67c9b4666c0d008
Kevin Krisna <kevinkrisnadp98@gmail.com>
2025-05-28 18:41:06 +0700

    remove unused repository

--- src/main/java/com/nostratech/m_tok/validation/BookingValidation.java
+++ src/main/java/com/nostratech/m_tok/validation/BookingValidation.java
@@ -2,9 +2,7 @@ package com.nostratech.m_tok.validation;
 
 import com.nostratech.m_tok.dto.request.BookingRequestDto;
 import com.nostratech.m_tok.persistence.model.Booking;
-import com.nostratech.m_tok.persistence.model.Seat;
 import com.nostratech.m_tok.persistence.repository.BookingRepository;
-import com.nostratech.m_tok.persistence.repository.SeatRepository;
 import com.nostratech.m_tok.utils.StatusEnum;
 import com.nostratech.m_tok.validation.annotation.BookingValid;
 import jakarta.validation.ConstraintValidator;
@@ -15,11 +13,9 @@ import java.util.List;
 public class BookingValidation implements ConstraintValidator<BookingValid, BookingRequestDto>  {
 
     private final BookingRepository bookingRepository;
-    private final SeatRepository seatRepository;
 
-    public BookingValidation(BookingRepository bookingRepository, SeatRepository seatRepository) {
+    public BookingValidation(BookingRepository bookingRepository) {
         this.bookingRepository = bookingRepository;
-        this.seatRepository = seatRepository;
     }
 
     @Override
